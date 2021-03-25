ed2d7d8955125aa1663055f31df2cd9016d5244a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-25 15:11:50 -0700

    front end receiving movie data in the console log! yayygit add .git add .

---
+++ src/movies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class Movies extends React.Component {
+  render() {
+    return(
+      <>
+           
+      </>
+    )
+  }
+}
+
+export default Movies;
